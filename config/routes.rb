20b0f2dab7061e1bc16db4a96404a433cd4bcd31
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-23 03:18:49 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "lion", :action => "vienna" })
+
+  get("/square/new", { :controller => "dog", :action => "chicago" })
+
+  get("/square/results", { :controller => "penguin", :action => "cairo" })
+
+  get("/square_root/new", { :controller => "elephant", :action => "amsterdam" })
+
+  get("/square_root/results", { :controller => "tiger", :action => "tokio" })
+
+  get("/payment/new", { :controller => "giraffe", :action => "nairobi" })
+
+  get("/payment/results", { :controller => "cat", :action => "madrid" })
+
+  get("/random/new", { :controller => "squirrel", :action => "rome" })
+
+  get("/random/results", { :controller => "seal", :action => "berlin" })
   
+
 end
