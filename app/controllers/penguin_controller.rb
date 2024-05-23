fa4eebc4037317df4c13a5067aa3f1ed341c8409
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-23 03:35:37 +0000

    square complete

--- app/controllers/penguin_controller.rb
+++ app/controllers/penguin_controller.rb
@@ -1,4 +1,7 @@
 class PenguinController < ApplicationController
-  def
+  def cairo
+    @the_num = params.fetch("users_number").to_f
+    @the_result = @the_num ** 2
+    render( { :template => "result/square"})
   end
 end
